0b634572db67ed7e2c162b773d3879acec824881
Yarin <yarinleib@gmail.com>
2025-02-01 15:41:08 +0100

    done

--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,23 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
-
 describe("Iteration 3", () => {
-    describe("Function - calculateArea", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
-
-    })    
-})
-
-*/
+  describe("Function - calculateArea", () => {
+    // Each `it` block represents 1 test. You can use the following as a template:
+    it("should be defind", () => {
+      expect(calculateArea).toBeDefined();
+    });
+    it("should take two arguments", () => {
+      expect(calculateArea.length).toBe(2);
+    });
+    it("should return the rectangle by multiplying the two numbers", () => {
+      expect(calculateArea(3, 3)).toEqual(9);
+      expect(calculateArea(10, 5)).toEqual(50);
+      expect(calculateArea(100, 10)).toEqual(1000);
+    });
+    it("should return underfind if any of the argumets is not a provided", () => {
+      expect(calculateArea()).toEqual(undefined);
+      expect(calculateArea(1)).toEqual(undefined);
+      expect(calculateArea(undefined, 1)).toEqual(undefined);
+    });
+  });
+});
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,23 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
-
 describe("Iteration 2", () => {
-    describe("Function - divide", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
-
-    })    
-})
-
-*/
+  describe("Function - divide", () => {
+    // Each `it` block represents 1 test. You can use the following as a template:
+    it("should be defind", () => {
+      expect(divide).toBeDefined();
+    });
+    it("should take two arguments", () => {
+      expect(divide.length).toBe(2);
+    });
+    it("should return the division of the two numbers", () => {
+      expect(divide(10, 5)).toEqual(2);
+      expect(divide(30, 5)).toEqual(6);
+      expect(divide(114, 3)).toEqual(38);
+    });
+    it("should return underfind if any of the argumets is not a provided", () => {
+      expect(divide(undefined, 1)).toEqual(undefined);
+      expect(divide()).toEqual(undefined);
+      expect(divide(1)).toEqual(undefined);
+    });
+  });
+});
